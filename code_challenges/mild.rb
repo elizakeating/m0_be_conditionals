77980daf18033f37be515a2a31d21f7b91260d50
Eliza Keating <ek.elizakeating@gmail.com>
2023-06-01 12:16:25 -0600

    Finish mild challenge

---
+++ code_challenges/mild.rb
@@ -0,0 +1,18 @@
+# Write a Ruby program that defines a variable that stores an Integer. The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+number = 125
+
+if number % 2 == 0
+    puts "even"
+else
+    puts "odd"
+end
+
+# or
+
+number = 14
+if number.even?
+    puts "even"
+else
+    puts "odd"
+end
